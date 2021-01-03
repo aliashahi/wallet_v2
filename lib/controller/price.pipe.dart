12f6f0a2762ac53eb69a2aead0feac2c4c8e8ecb
Ali Akbar Shahi <ali.akbar.shahi.junior@gmail.com>
2021-01-03 21:45:49 +0330

    build and vector fixed-- gradle updated

--- lib/controller/price.pipe.dart
+++ lib/controller/price.pipe.dart
@@ -1,6 +1,6 @@
 import 'package:intl/intl.dart';
 
 String priceConvertor(int amount) {
-  NumberFormat formatter = new NumberFormat('#,##,000');
+  NumberFormat formatter = new NumberFormat('###,###,###,###,###,000');
   return formatter.format(amount);
 }
